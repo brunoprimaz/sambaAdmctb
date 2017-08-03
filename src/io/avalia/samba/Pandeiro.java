8adf708de7ece539ee0a2c00dd3271c063b1839b
Edmar <hotmaildoed@hotmail.com>
2017-08-03 13:52:56 -0300

    correcao do teste

---
+++ src/io/avalia/samba/Pandeiro.java
@@ -0,0 +1,17 @@
+package io.avalia.samba;
+
+public class Pandeiro implements IInstrument {
+
+	public String play() {
+		return "papa";
+	}
+
+	public int getSoundVolume() {
+		return 16;
+	}
+
+	public String getColor() {
+		return "white";
+	}
+
+}
